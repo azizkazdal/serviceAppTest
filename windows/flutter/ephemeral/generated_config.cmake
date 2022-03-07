5bd00356e490d78912dfc1cf75569d4d638f8255
S.Aziz Kazdal <azizkazdal@gmail.com>
2022-03-07 19:32:04 +0300

    1931

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Source\\EQLEM\\src\\PublicApps\\service_request_test" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Source\\EQLEM\\src\\PublicApps\\service_request_test"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Source\\EQLEM\\src\\PublicApps\\service_request_test\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Source\\EQLEM\\src\\PublicApps\\service_request_test"
+  "FLUTTER_TARGET=C:\\Source\\EQLEM\\src\\PublicApps\\service_request_test\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Source\\EQLEM\\src\\PublicApps\\service_request_test\\.dart_tool\\package_config.json"
+)
